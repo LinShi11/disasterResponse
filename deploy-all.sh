a0022d16806de4e943b028e76623b2206e1a00cb
mahidharareddy <mahidhar2099@gmail.com>
2023-12-13 13:26:38 -0700

    Modify Makefile

--- deploy-all.sh
+++ deploy-all.sh
@@ -1,13 +1,16 @@
 kubectl apply -f rabbitMQ/rabbitmq-deployment.yaml
 kubectl apply -f rabbitMQ/rabbitmq-service.yaml
+sleep 60
 
 kubectl apply -f frontend/frontend-deployment.yaml
 kubectl apply -f frontend/frontend-service.yaml
 kubectl apply -f frontend/frontend-ingress.yaml
+sleep 30
 
 kubectl apply -f backend/backend-deployment.yaml
 kubectl apply -f backend/backend-service.yaml
 
-kubectl port-forward backend-deployment 5000:5000 &
-kubectl port-forward frontend-deployment 3000:3000 &
-kubectl port-forward rabbitmq-deployment 5672:5672 &
+sleep 10
+kubectl port-forward deployments/backend-deployment 5000:5000 &
+kubectl port-forward deployments/frontend-deployment 3000:3000 &
+kubectl port-forward deployments/rabbitmq-deployment 5672:5672 &
